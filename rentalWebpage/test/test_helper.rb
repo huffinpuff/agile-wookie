77d6c4aa165cbeb0679056edbd834f39a743583b
dee dee <3dsMaxdee@gmail.com>
2015-04-18 02:23:22 +0000

    install a test to see if a valid state has been entered

--- rentalWebpage/test/test_helper.rb
+++ rentalWebpage/test/test_helper.rb
@@ -1,8 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-class ActiveSupport::TestCase
+# Test to see if there is a valid state entered.
+class StateTest < ActiveSupport::TestCase
+  test "creates a valid record" do
+    state = State.new 
+    state.location = "NO"
+    assert state.save
+  end
+  
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
